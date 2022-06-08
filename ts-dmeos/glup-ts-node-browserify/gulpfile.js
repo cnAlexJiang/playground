35737073a06b2c23e6d4e459226630ec635dd0dd
alex <jiangyi@corp-ci.com>
2022-06-08 20:53:55 +0800

    glup玩具

--- ts-dmeos/glup-ts-node-browserify/gulpfile.js
+++ ts-dmeos/glup-ts-node-browserify/gulpfile.js
@@ -1,35 +1,31 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var watchify = require("watchify");
+var tsify = require("tsify");
+var fancy_log = require("fancy-log");
 var paths = {
-    pages: ['src/*.html']
+  pages: ["src/*.html"],
 };
-
-gulp.task('copyHtml', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
+var watchedBrowserify = watchify(
+  browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["src/main.ts"],
+    cache: {},
+    packageCache: {},
+  }).plugin(tsify)
+);
+gulp.task("copy-html", function () {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
-
-gulp.task('default', ['copyHtml'], function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-    .plugin(tsify)
-    .transform('babelify', {
-        presets: ['es2015'],
-        extensions: ['.ts']
-    })
+function bundle() {
+  return watchedBrowserify
     .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
-});
+    .on("error", fancy_log)
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("dist"));
+}
+gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", fancy_log);
---
+++ ts-dmeos/glup-ts-node-browserify/readME.md
@@ -0,0 +1,2 @@
+## 配置参考
+  - https://www.typescriptlang.org/docs/handbook/gulp.html#browserify
