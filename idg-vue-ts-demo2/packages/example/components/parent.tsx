cc2eef7f411a750327f2fdffc8653563912e97da
alex <jiangyi@corp-ci.com>
2022-08-22 22:24:55 +0800

    1

---
+++ idg-vue-ts-demo2/packages/example/components/parent.tsx
@@ -0,0 +1,22 @@
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Button } from '@idg/iview';
+@Component({
+  depends: ['api.TaskApi'],
+})
+export default class parent extends Vue {
+  @Prop({ type: String, default: 'aaa' })
+  public x: string = '123';
+  created() {
+    console.log('i am parent');
+  }
+  public render() {
+    return (
+      <div class='flex justify-center items-center'>
+        <p class='mr-6'>test-!</p>
+        <div>
+          <Button>{this.x}</Button>
+        </div>
+      </div>
+    );
+  }
+}
