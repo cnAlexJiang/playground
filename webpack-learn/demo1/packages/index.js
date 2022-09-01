0f19053ffe1ee185e49c351e6c3b1847417d0109
alex <jiangyi@corp-ci.com>
2022-09-01 23:18:20 +0800

    1

--- webpack-learn/demo1/packages/index.js
+++ webpack-learn/demo1/packages/index.js
@@ -1,28 +1,4 @@
 import './base.less';
-require('@babel/polyfill')
-import {setTimeoutTest} from './test-esm'
-
-import {a,add} from './test-esm'
-
-;(async function(){
-  console.log('111')
-  await setTimeoutTest()
-  console.log('222')
-
-}())
-
-
-let module1 = require('./test-cmj');
-console.log('module1', module1);
-let module2= require('./test-cmj');
-
-
-console.log('esm', a, add(1,2), a)
-console.log('cmj111',module1, module1.a, module1.add(1,2), module1.a )
-console.log('cm222',module2, module2.a, module2.add(1,2), module2.a )
-
-
-
 
 
 
