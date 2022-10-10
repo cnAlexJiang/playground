f6657e1cd06565a29643c01ccdef833521c9d298
alex <jiangyi@corp-ci.com>
2022-10-10 17:47:25 +0800

    mac

--- vite-playground/vue2-demo1/src/components/TestA.vue
+++ vite-playground/vue2-demo1/src/components/TestA.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>TestA</div>
+  <div class="testa" @contextmenu="handler">TestA</div>
 </template>
 <script>
 export default {
   created() {
     console.log('TestA created')
   },
+  methods: {
+    handler (e) {
+      e.preventDefault();
+      return false
+    }
+  }
 }
 </script>
 <style scoped>
-.read-the-docs {
-  color: #888;
+.testa{
+  width: 400px;
+  height: 400px;
+  border: 1px solid red;
 }
 </style>
