da973c8036594f12ae40aa6152f598b520ed7c49
alex <cnalexj@gmail.com>
2022-10-30 22:35:05 +0800

    store & router

--- vite-playground/vue3-demo1/src/App2.vue
+++ vite-playground/vue3-demo1/src/App2.vue
@@ -12,6 +12,17 @@
     <TestHook></TestHook>
     <TestTransition />
     <TestTeleport />
+    <br>
+    <TestRouter />
+    <br>
+    <TestStore />
+    <br>
+    <div>
+      <button @click="globalCount++">globalCount-{{ globalCount }}</button>
+    </div>
+    <div>
+      <button @click="localCount++">localCount-{{ localCount }}</button>
+    </div>
   </div>
 </template>
 <script setup>
@@ -21,6 +32,14 @@ import TestF from './components/TestF.vue';
 import TestHook from './components/TestHook.vue';
 import TestTransition from './components/TestTransition.vue'
 import TestTeleport from './components/TestTeleport.vue'
+import TestRouter from './components/TestRouter.vue'
+import TestStore from './components/TestStore.vue'
+
+import { useCount } from './store/testHook'
+
+const { globalCount, localCount } = useCount()
+
+
 </script>
 <style scoped>
 
---
+++ vite-playground/vue3-demo1/src/components/TestStore.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+    <button @click="store.count++">
+        {{ store.count }}
+    </button>
+</template>
+<script setup>
+
+import { store } from '../store/app'
+
+
+</script>
+<style scoped>
+
+</style>
+  
