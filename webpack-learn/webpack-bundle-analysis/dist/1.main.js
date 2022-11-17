98cf03fc8f8104b561ed7076021f8ead84a1fb75
jiangyi <jiangyi@corp-ci.com>
2022-11-17 20:17:53 +0800

    java-hello

---
+++ webpack-learn/webpack-bundle-analysis/dist/1.main.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./src/info2.js":
+/*!**********************!*\
+  !*** ./src/info2.js ***!
+  \**********************/
+/*! exports provided: log */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"log\", function() { return log; });\nconst log = {\n  log2 : \"log info2\"\n}\n\n//# sourceURL=webpack:///./src/info2.js?");
+
+/***/ })
+
+}]);
