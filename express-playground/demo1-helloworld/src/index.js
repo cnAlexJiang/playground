ad0eeadae859110a5d229b3bc98f73393c4e9574
alex <cnalexj@gmail.com>
2022-11-23 10:42:23 +0800

    11

---
+++ express-playground/demo1-helloworld/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/user', function (req, res) {
+    const data = {
+        name: 'aa',
+        age: 22,
+    }
+    res.send(data)
+
+    // res.send(JSON.stringify(data))
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
