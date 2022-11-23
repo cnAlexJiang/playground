32840239a15ae48aaf5f144fde555df3a6aa4616
jiangyi <jiangyi@corp-ci.com>
2022-11-23 22:15:22 +0800

    redux

--- react-playground/create-react-app-2/src/index.js
+++ react-playground/create-react-app-2/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,16 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+store.subscribe(()=>{
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+})
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
