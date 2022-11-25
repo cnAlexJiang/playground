8ab6e82af081574119b38ca5e39e8489beb424a5
alex <cnalexj@gmail.com>
2022-11-25 10:17:19 +0800

    redux-easy

--- react-playground/react-demo4-redux-easy/src/features/counter/Counter.js
+++ react-playground/react-demo4-redux-easy/src/features/counter/Counter.js
@@ -11,12 +11,13 @@ import {
 import styles from './Counter.module.css';
 
 export function Counter() {
+
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
   const incrementValue = Number(incrementAmount) || 0;
-
+  console.log('call function Counter')
   return (
     <div>
       <div className={styles.row}>
--- react-playground/react-demo4-redux-easy/src/features/counter/counterSlice.js
+++ react-playground/react-demo4-redux-easy/src/features/counter/counterSlice.js
@@ -14,6 +14,7 @@ const initialState = {
 export const incrementAsync = createAsyncThunk(
   'counter/fetchCount',
   async (amount) => {
+    console.log(333, amount)
     const response = await fetchCount(amount);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
@@ -59,7 +60,10 @@ export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectCount = (state) => {
+  console.log(222, state)
+  return state.counter.value
+};
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
