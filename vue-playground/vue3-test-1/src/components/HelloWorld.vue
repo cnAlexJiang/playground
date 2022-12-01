4d0a3555f3465fbfa7842592c3b527c5fde3a0cf
alex <cnalexj@gmail.com>
2022-12-01 20:37:53 +0800

    vue-use

---
+++ vue-playground/vue3-test-1/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import aa from './a.vue'
+import bb from './b.vue'
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <aa></aa>
+    <bb></bb>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ vue-playground/vue3-test-1/src/components/a.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useFetchList } from '../api/index'
+let { list } = useFetchList()
+</script>
+
+<template>
+    <h1>aa = {{ list }}</h1>
+</template>
+
+ 
