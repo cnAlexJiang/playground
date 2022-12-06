57537e25f7d46685e5cd28961de59f7e0ba461fa
alex <cnalexj@gmail.com>
2022-12-07 02:30:07 +0800

    ng start

---
+++ angular-playground/demo1-ag8/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public counter: number = 0;
+
+  constructor() { }
+
+  public increment() {
+    this.counter++;
+    return this.counter;
+  }
+
+  public decrement() {
+    this.counter--;
+    return this.counter;
+  }
+
+  public reset() {
+    this.counter = 0;
+    return this.counter;
+  }
+}
