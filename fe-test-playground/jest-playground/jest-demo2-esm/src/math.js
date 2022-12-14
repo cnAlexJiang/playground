c33a0141f3b969188c646927d91a3f6831314188
jiangyi <jiangyi@corp-ci.com>
2022-12-14 16:50:13 +0800

    test-playground

---
+++ fe-test-playground/jest-playground/jest-demo2-esm/src/math.js
@@ -0,0 +1,13 @@
+// math.js
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function minus(a, b) {
+  return a - b;
+}
+
+export function multi(a, b) {
+  return a * b;
+}
---
+++ fe-test-playground/jest-playground/readME.md
@@ -0,0 +1,2 @@
+## 参考
+ - https://juejin.cn/post/7066792153027969032#heading-30  snapshot 快照测试、mock timers、ES6中类的测试、对 DOM 节点的测试。
