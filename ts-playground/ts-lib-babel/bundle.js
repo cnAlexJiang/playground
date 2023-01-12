65fa287992a40bf0c2995f9a26097eebc28e397d
jiangyi <jiangyi@corp-ci.com>
2023-01-12 14:49:45 +0800

    ts-playground-1

---
+++ ts-playground/ts-lib-babel/bundle.js
@@ -0,0 +1,7 @@
+export function main() {
+  const res = add(1, 1);
+  console.log('res=', res);
+}
+export function add(a, b) {
+  return a + b;
+}
---
+++ ts-playground/ts-web-tsify/main.ts
@@ -0,0 +1,11 @@
+
+export function main(){
+  const res= add(1,1)
+  console.log('res=', res)
+}
+
+export function add(a:number, b:number){
+  return a+b
+}
+
+console.log('hello world')
