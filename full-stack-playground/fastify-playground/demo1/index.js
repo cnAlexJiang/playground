8fe9cf57a56a456a3c73eb37ea5305446600ef71
jiangyi <jiangyi@corp-ci.com>
2023-02-02 11:10:24 +0800

    move file

---
+++ full-stack-playground/fastify-playground/demo1/index.js
@@ -0,0 +1,21 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+fastify.get('/data', async (request, reply) => {
+  return { aa: 'aa', bb: 'bb'}
+})
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
