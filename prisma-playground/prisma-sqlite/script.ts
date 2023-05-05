070c5a6941f68ca5fa70d0400f5032a299e011b5
jiangyi <jiangyi@corp-ci.com>
2023-05-05 15:56:52 +0800

    prisma-sqlite+ mysql

---
+++ prisma-playground/prisma-sqlite/script.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // step1 创建
+
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //   },
+  // })
+  // console.log(user)
+
+  // step2 检索
+  // const users = await prisma.user.findMany()
+  // console.log(users)
+
+  // step3 relation queries
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Bob',
+  //     email: 'bob@prisma.io',
+  //     posts: {
+  //       create: {
+  //         title: 'Hello World',
+  //       },
+  //     },
+  //   },
+  // })
+  // console.log(user)
+
+  // const users = await prisma.user.findMany()
+  // console.log(users)
+
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(usersWithPosts, { depth: null })
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
---
+++ prisma-playground/prisma.md
@@ -0,0 +1,65 @@
+## quick start
+ 
+
+### 创建 ts项目 安装  prisma
+```
+// 1
+mkdir hello-prisma 
+cd hello-prisma
+
+// 2 
+
+npm init -y
+npm install typescript ts-node @types/node  -D
+
+// 3 
+
+npx tsc --init 
+
+npm install prisma  -D
+
+npx prisma init --datasource-provider xxxdatabase
+
+```
+
+
+###  数据迁移
+```
+npx prisma migrate dev --name init
+
+// 结果会创建 migrations
+```
+
+### 业务侧
+```
+touch script.ts
+
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+
+// exec
+
+npx ts-node script.ts
+```
+
+## 查看数据库
+```
+npx prisma studio
+```
